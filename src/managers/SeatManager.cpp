800d2aef1b854848d2241eefc4b96243fbb046d7
Vaxry <43317083+vaxerski@users.noreply.github.com>
2024-07-21 13:09:54 +0200

    Core: Move to aquamarine (#6608)
    Moves Hyprland from wlroots to aquamarine for the backend.
    
    ---------
    
    Signed-off-by: Vaxry <vaxry@vaxry.net>
    Co-authored-by: Mihai Fufezan <mihai@fufexan.net>
    Co-authored-by: Jan Beich <jbeich@FreeBSD.org>
    Co-authored-by: vaxerski <vaxerski@users.noreply.github.com>
    Co-authored-by: UjinT34 <41110182+UjinT34@users.noreply.github.com>
    Co-authored-by: Tom Englund <tomenglund26@gmail.com>
    Co-authored-by: Ikalco <73481042+ikalco@users.noreply.github.com>
    Co-authored-by: diniamo <diniamo53@gmail.com>

--- src/managers/SeatManager.cpp
+++ src/managers/SeatManager.cpp
@@ -94,8 +94,8 @@ void CSeatManager::setKeyboard(SP<IKeyboard> KEEB) {
 }
 
 void CSeatManager::updateActiveKeyboardData() {
-    if (keyboard && keyboard->wlr())
-        PROTO::seat->updateRepeatInfo(keyboard->wlr()->repeat_info.rate, keyboard->wlr()->repeat_info.delay);
+    if (keyboard)
+        PROTO::seat->updateRepeatInfo(keyboard->repeatRate, keyboard->repeatDelay);
     PROTO::seat->updateKeymap();
 }
 
@@ -103,7 +103,7 @@ void CSeatManager::setKeyboardFocus(SP<CWLSurfaceResource> surf) {
     if (state.keyboardFocus == surf)
         return;
 
-    if (!keyboard || !keyboard->wlr()) {
+    if (!keyboard) {
         Debug::log(ERR, "BUG THIS: setKeyboardFocus without a valid keyboard set");
         return;
     }
@@ -144,7 +144,7 @@ void CSeatManager::setKeyboardFocus(SP<CWLSurfaceResource> surf) {
                 continue;
 
             k->sendEnter(surf);
-            k->sendMods(keyboard->wlr()->modifiers.depressed, keyboard->wlr()->modifiers.latched, keyboard->wlr()->modifiers.locked, keyboard->wlr()->modifiers.group);
+            k->sendMods(keyboard->modifiersState.depressed, keyboard->modifiersState.latched, keyboard->modifiersState.locked, keyboard->modifiersState.group);
         }
     }
 
@@ -191,7 +191,12 @@ void CSeatManager::setPointerFocus(SP<CWLSurfaceResource> surf, const Vector2D& 
     if (state.pointerFocus == surf)
         return;
 
-    if (!mouse || !mouse->wlr()) {
+    if (PROTO::data->dndActive() && surf) {
+        Debug::log(LOG, "[seatmgr] Refusing pointer focus during an active dnd");
+        return;
+    }
+
+    if (!mouse) {
         Debug::log(ERR, "BUG THIS: setPointerFocus without a valid mouse set");
         return;
     }
